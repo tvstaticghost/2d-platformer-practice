c4116b0b6fac99d2f44d9d290fa60550e7f698a4
Ray Arnold <rarno93@wgu.edu>
2025-06-18 21:02:55 -0400

    adding tile generation logic

--- Assets/Scripts/TileGenerator.cs
+++ Assets/Scripts/TileGenerator.cs
@@ -4,6 +4,9 @@ using UnityEngine.Tilemaps;
 public class TileGenerator : MonoBehaviour
 {
     public Tilemap tilemap;
+    public Tilemap redTileMap;
+    public TileBase redTile;
+    private Vector3Int targetPos;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,6 +19,15 @@ public class TileGenerator : MonoBehaviour
                 Debug.Log($"Tile found at grid position: {pos}");
             }
         }
+
+        //Draw tile logic - I DID IT!
+        targetPos = new Vector3Int(7, -5, 0);
+        redTileMap.SetTile(targetPos, redTile);
+    }
+
+    void GenerateTileSet()
+    {
+
     }
 
     // Update is called once per frame
